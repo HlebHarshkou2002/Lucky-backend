8393da5cb2b68c3680f8c218ac77b697d2deb069
unknown <erichkrause2012@tut.by>
2024-10-21 14:52:18 +0300

    added email to orders

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,4 +1,40 @@
 import OrderModel from "../models/Order.js";
+import nodemailer from "nodemailer"
+import { google } from "googleapis"
+
+
+
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+
+async function sendMail(message) {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken()
+
+    //MAIL SENDING
+
+    let transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: "OAuth2",
+        user: "hlebharshkou@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken
+      },
+    });
+
+    const result = await transport.sendMail(message)
+
+    return result
+  } catch (error) {
+    return error
+  }
+}
+
+//MAIL SENDING ENDS
 
 export const getAll = async (req, res) => {
   try {
@@ -68,10 +104,41 @@ export const create = async (req, res) => {
 
     const doc = new OrderModel({
       user: req.body.userId,
-      products: req.body.products
+      products: req.body.products,
+      pickupPoint: req.body.pickUpPoint,
+      phone: req.body.phone,
+      comment: req.body.comments,
+      paymentMethod: req.body.paymentOption,
+      contactEmail: req.body.email,
+      contactFullName: req.body.fullName,
+      deliveryLocation: req.body.pickUpPoint
+
     });
 
     const order = await doc.save();
+
+    const adresses = {
+      "point1": "г. Минск, ул. Рафиева, д. 42",
+      "point2": "г. Минск, прт. Дзержинского, д. 111"
+    }
+
+    //MAIL SENDING
+    const message = {
+      from: 'LUCKY SHOP <hlebharshkou2002@mail.ru>',
+      to: req.body.email,
+      subject: 'Congrats!',
+      text: 'Поздравляем! Вы оформили заказ',
+      html:
+        `<div>
+            <h1>Поздравляем! ${req.body.fullName}, Вы оформили заказ в магазине Lucky!\n</h1>
+            <p>Забрать заказ вы можете по адресу ${adresses[req.body.pickUpPoint]}.\n</p>
+            <p>Статус заказа вы можете посмотреть в <a href="http://localhost:3000/profile">личном кабинете</a></p>
+        </div>`,
+    }
+
+    sendMail(message)
+      .then((result) => console.log("Email sent...", result))
+      .catch((error) => console.log(error.message))
 
     res.json(order);
   } catch (err) {
--- models/Order.js
+++ models/Order.js
@@ -11,6 +11,38 @@ const OrderSchema = new mongoose.Schema({
         type: String,
         default: "process" //process, ready, paid
     },
+    deliveryLocation: {
+        type: String,
+        default: "" 
+    },
+    pickupPoint: {
+        type: String,
+        default: "" 
+    },
+    phone: {
+        type: String,
+        default: ""
+    },
+    comment: {
+        type: String,
+        default: "" 
+    },
+    paymentMethod: {
+        type: String,
+        default: "" //cash, website, card by receive
+    },
+    paymentStatus: {
+        type: String,
+        default: "unpaid" //unpaid, paid
+    },
+    contactEmail: {
+        type: String,
+        default: ""
+    },
+    contactFullName: {
+        type: String,
+        default: ""
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
